8c6a555aa071bdec92137529a652d31d79efb166
Mika Sormunen <sormmi@gmail.com>
2019-08-15 23:53:03 +0300

    Add CoinTile

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import {AppContext} from "../App/AppProvider";
-import {SelectableTile} from "../Shared/Tile";
+import CoinTile from "./CoinTile";
 
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     grid-gap: 15px;
+    margin-top: 40px;
 `;
+
+const getCoins = (coinList) => {
+    return Object.keys(coinList).slice(0, 100);
+};
+
 const CoinGrid = props => {
     return (
         <AppContext.Consumer>
             {({coinList}) =>
                 <CoinGridStyled>
-                    {Object.keys(coinList).map(coinKey => (
-                        <SelectableTile key={coinKey}>{coinKey}</SelectableTile>
+                    {getCoins(coinList).map(coinKey => (
+                        <CoinTile coinKey={coinKey}/>
                     ))}
                 </CoinGridStyled>
             }
---
+++ src/Settings/CoinTile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {AppContext} from "../App/AppProvider";
+import {SelectableTile} from "../Shared/Tile";
+import CoinHeaderGrid from "./CoinHeaderGrid";
+import CoinImage from "./CoinImage";
+
+const CoinTile = ({coinKey}) => {
+    return (
+        <AppContext.Consumer>
+            {({coinList}) => {
+                let coin = coinList[coinKey];
+
+                const TileClass = SelectableTile;
+
+                return (
+                    <TileClass>
+                        <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol}/>
+                        <CoinImage coin={coin}/>
+                    </TileClass>
+                );
+            }}
+        </AppContext.Consumer>
+    );
+};
+
+export default CoinTile;
