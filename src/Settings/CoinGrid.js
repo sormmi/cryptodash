f6d2a6c3d226314cc7946583463e3e35def85b10
Mika Sormunen <sormmi@gmail.com>
2019-08-16 19:23:46 +0300

    Finish add/remove coins

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -10,17 +10,17 @@ export const CoinGridStyled = styled.div`
     margin-top: 40px;
 `;
 
-const getCoins = (coinList, topSection) => {
-    return Object.keys(coinList).slice(0, topSection ? 10 : 100);
+const getCoins = (coinList, topSection, favorites) => {
+    return topSection ? favorites : Object.keys(coinList).slice(0, 100);
 };
 
 const CoinGrid = ({topSection}) => {
     return (
         <AppContext.Consumer>
-            {({coinList}) =>
+            {({coinList, favorites}) =>
                 <CoinGridStyled>
-                    {getCoins(coinList, topSection).map(coinKey => (
-                        <CoinTile topSection={topSection} coinKey={coinKey}/>
+                    {getCoins(coinList, topSection, favorites).map((coinKey, index) => (
+                        <CoinTile key={index} topSection={topSection} coinKey={coinKey}/>
                     ))}
                 </CoinGridStyled>
             }
--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -4,19 +4,28 @@ import {DeletableTile, DisabledTile, SelectableTile} from "../Shared/Tile";
 import CoinHeaderGrid from "./CoinHeaderGrid";
 import CoinImage from "./CoinImage";
 
+const onClickCoinHandler = (topSection, coinKey, addCoin, removeCoin) => {
+    return topSection ? () => {
+        removeCoin(coinKey);
+    } : () => {
+        addCoin(coinKey);
+    };
+};
+
 const CoinTile = ({coinKey, topSection}) => {
     return (
         <AppContext.Consumer>
-            {({coinList}) => {
+            {({coinList, addCoin, removeCoin, isInFavorites}) => {
                 let coin = coinList[coinKey];
 
                 let TileClass = SelectableTile;
                 if (topSection) {
                     TileClass = DeletableTile;
+                } else if (isInFavorites(coinKey)) {
+                    TileClass = DisabledTile;
                 }
-                const e = DisabledTile;
                 return (
-                    <TileClass>
+                    <TileClass onClick={onClickCoinHandler(topSection, coinKey, addCoin, removeCoin)}>
                         <CoinHeaderGrid topSection={topSection} name={coin.CoinName} symbol={coin.Symbol}/>
                         <CoinImage coin={coin}/>
                     </TileClass>
